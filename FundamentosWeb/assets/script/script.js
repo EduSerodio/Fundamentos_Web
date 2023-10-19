919cfc99132540fb53afb3bdb8a8be719433aa51
EduSerodio <dudu9090@outlook.com.br>
2023-10-19 16:36:49 -0300

    Implementando validação no formulario e adicionando algumas animações na página de contato

--- FundamentosWeb/assets/script/script.js
+++ FundamentosWeb/assets/script/script.js
@@ -1,6 +1,9 @@
 let nome = document.getElementById('name-input')
 let email = document.getElementById('email-input')
 let telefone = document.querySelector('#telefone-input')
+let mapa = document.querySelector('#mapa')
+
+
 
 const form = document.getElementById("form")
 
@@ -16,5 +19,47 @@ function validacaoForms() {
     }else{
         alert("Dados cadastrados!!")
     }
-    
+}
+
+function validaNome() {
+    const txtNome = document.querySelector('#txtNome')
+    if(nome.value.length < 3){
+        txtNome.innerHTML = 'Nome Inválido'
+        txtNome.style.color = 'red'
+    }else {
+        txtNome.innerHTML = 'Nome Válido'
+        txtNome.style.color = 'green'
+    }
+}
+
+function validaEmail(){
+    const txtEmail = document.querySelector('#txtEmail')
+    if(email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1) {
+        txtEmail.innerHTML = 'E-mail inválido'
+        txtEmail.style.color = 'red'
+    }else {
+        txtEmail.innerHTML = 'E-mail válido'
+        txtEmail.style.color = 'green'
+    }
+}
+
+function validaTelefone(){
+    const txtTelefone = document.querySelector('#txtTelefone')
+    if(telefone.value.length == 11) {
+        txtTelefone.innerHTML = 'Numero válido'
+        txtTelefone.style.color = 'green'
+    }else {
+        txtTelefone.innerHTML = 'Numero inválido'
+        txtTelefone.style.color = 'red'
+    }
+}
+
+function mapaZoom() {
+    mapa.style.width = '800px'
+    mapa.style.height = '600px'
+}
+
+function mapaNormal() {
+    mapa.style.width = '500px'
+    mapa.style.height = '350px'
 }
